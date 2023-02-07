d71f72cdaf8d46e6f382b34aa71799decfb2e5a0
Hernan <hschiavonr@gmail.com>
2023-02-07 22:03:54 +0100

    fix menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,34 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import "./Header.scss"
 
 
 const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen)
+  }
+
   return (
     <div className='header'>
-    {/* <img src={logo} className="header__logo" alt='logo'></img> */}
-    <div>
-      <Link to="/">
-      <img
-          className="header__logo"
-          src="https://viniloslowcost.es/7460/vinilo-escudo-argentina-national-team-football.jpg"
-          alt="First slide"
-        />
-      </Link>
-    </div>
-    
-    <div className='header__menu'>
+      {/* <img src={logo} className="header__logo" alt='logo'></img> */}
+      <div>
+        <Link to="/">
+        <img
+            className="header__logo"
+            src="https://viniloslowcost.es/7460/vinilo-escudo-argentina-national-team-football.jpg"
+            alt="First slide"
+          />
+        </Link>
+      </div>
+      <div className='header__menu'>
         <ul className='header__menu--list'>
-            <li className='header__menu--list--list1'><Link to="/"><button className='bn3637 bn37'><span className='header__menu--list__titulo'>Home</span></button></Link></li>
-            <li className='header__menu--list--list1'><Link to="/ClausulaSuelo"><button className='bn3637 bn37'><span className='header__menu--list__titulo'>Clausula Suelo</span></button></Link></li>
-            <li className='header__menu--list--list1'><Link to="/Creditos"><button className='bn3637 bn37'><span className='header__menu--list__titulo'>Creditos</span></button></Link></li>
-            <li className='header__menu--list--list1'><Link to="/GastosHipoteca"><button className='bn3637 bn37'><span className='header__menu--list__titulo'>Gastos Hipoteca</span></button></Link></li>
-            <li className='header__menu--list--list1'><Link to="/Multidivisa"><button className='bn3637 bn37'><span className='header__menu--list__titulo'>Multidivisa</span></button></Link></li>           
-            <li className='header__menu--list--list1'><Link to="/TarjetasRevolving"><button className='bn3637 bn37'><span className='header__menu--list__titulo'>Tarjetas Revolving</span></button></Link></li>
+          <li className='header__menu--list--list1'>
+            <button className='bn3637 bn37' onClick={toggleMenu}>
+              <span className='header__menu--list__titulo'>Menu</span>
+            </button>
+            {isMenuOpen && (
+              <ul className='header__sub-menu'>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/ClausulaSuelo">Clausula Suelo</Link></li>
+                <li><Link to="/Creditos">Creditos</Link></li>
+                <li><Link to="/GastosHipoteca">Gastos Hipoteca</Link></li>
+                <li><Link to="/Multidivisa">Multidivisa</Link></li>           
+                <li><Link to="/TarjetasRevolving">Tarjetas Revolving</Link></li>
+              </ul>
+            )}
+          </li>
         </ul>
-    </div>
-
+      </div>
     </div>
   )
 }
